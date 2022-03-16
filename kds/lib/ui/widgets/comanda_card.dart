c551b0133d9146f630fafcbd893bffddd9d294ab
diana1 <gonzalez.sudia21@triana.salesianos.edu>
2022-03-16 15:27:45 +0100

    Se maquetó de diseñar el item de la tarjeta de comanda

--- kds/lib/ui/widgets/comanda_card.dart
+++ kds/lib/ui/widgets/comanda_card.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:kds/ui/styles/styles.dart';
 
 class ComandaCard extends StatefulWidget {
   const ComandaCard({Key? key}) : super(key: key);
@@ -13,55 +14,112 @@ class _ComandaCardState extends State<ComandaCard> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.blue,
+     
+      margin: EdgeInsets.all(10),
+      color: Colors.green,
       width: 300,
       child: Column(
         mainAxisSize: MainAxisSize.min,
         children: [
           Container(
+            
             child: Column(
               children: [
-                Row(
+                Container(
+                  margin: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+                  child: Row(
+                    
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [Text('10 min.'), Text('1/5')],
-                ),
+                  children: [Text('10 min.', style: TextStyle(color: Colors.white),), Text('1/5', style: TextStyle(color: Colors.white),)],
+                ),),
                 Container(
                   child: Row(
                     mainAxisAlignment: MainAxisAlignment.center,
-                    children: [Icon(Icons.person), Text('Nombre')],
+                    children: [
+                      Icon(
+                        Icons.person,
+                        color: Colors.white,
+                      ),
+                      Text(
+                        'Nombre',
+                        style: TextStyle(color: Colors.white),
+                      )
+                    ],
                   ),
                 ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Row(
-                      children: [Icon(Icons.push_pin), Text('1')],
-                    ),
-                    Container(
-                        child: Ink(
-                      decoration: const ShapeDecoration(
-                          color: Colors.black, shape: CircleBorder()),
-                      child: IconButton(
-            icon: const Icon(Icons.info),
-            color: Colors.white,
-            onPressed: () {},
-          ),
-                    ))
-                  ],
+                Container(
+                  margin: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Row(
+                        children: [Icon(Icons.push_pin, color: Colors.white,), Text('1', style: TextStyle(color: Colors.white),)],
+                      ),
+                      Container(
+                        width: 40,
+                        height: 30,
+                        child: TextButton(
+                          style: TextButton.styleFrom(
+                              backgroundColor: Colors.white,
+                              primary: Color.fromARGB(255, 71, 71, 71)),
+                          child: Icon(Icons.info),
+                          onPressed: () {},
+                        ),
+                      )
+                    ],
+                  ),
                 )
               ],
             ),
           ),
           Container(
+            margin: EdgeInsets.symmetric(horizontal: 2),
             child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [Icon(Icons.play_arrow), Icon(Icons.print)],
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Container(
+                  width: 195,
+                  height: 50,
+                  child: TextButton.icon(
+                    style: TextButton.styleFrom(
+                        backgroundColor: Colors.white, primary: Color.fromARGB(255, 87, 87, 87), textStyle: TextStyle(fontSize: 18)),
+                    onPressed: () {},
+                    icon: Icon(
+                      Icons.play_arrow,
+                      color: Color.fromARGB(255, 54, 115, 194), size: 30,
+                    ),
+                    label: Text(
+                      'Preparar',
+                    ),
+                  ),
+                ),
+                Container(
+                  width: 100,
+                  height: 50,
+                  child: TextButton(
+                    style: TextButton.styleFrom(
+                        backgroundColor: Colors.white, primary: Color.fromARGB(255, 87, 87, 87)),
+                    child: Icon(Icons.print),
+                    onPressed: () {},
+                  ),
+                )
+              ],
             ),
           ),
           Container(
-            child: Text('URGENTE'),
+            width: MediaQuery.of(context).size.width,
+            color: Color.fromARGB(255, 223, 60, 49),
+            alignment: Alignment.center,
+            height: 50,
+            //Hacer condición de que solo sale este espacio si se da tap en el botón de urgente
+            child: Text('¡¡¡URGENTE!!!', style: TextStyle(fontSize: 25, color: Colors.white),),
           ),
           Container(
+            color: Color.fromARGB(255, 241, 241, 241),
+            margin: EdgeInsets.only(left: 1, right: 1, bottom: 1),
+            width: MediaQuery.of(context).size.width,
+            alignment: Alignment.center,
+            height: 50,
             child: Text('Comandas'),
           )
         ],
