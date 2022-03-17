b39348c3fdbdd537107c701cac70ca567dbc52e0
diana1 <gonzalez.sudia21@triana.salesianos.edu>
2022-03-17 12:54:15 +0100

    Se eliminó código basura y se añadieron algunas vistas

--- kds/lib/ui/widgets/error_screen.dart
+++ kds/lib/ui/widgets/error_screen.dart
@@ -1,12 +1,27 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class ErrorScreen extends StatelessWidget {
-  const ErrorScreen({ Key? key }) : super(key: key);
+  const ErrorScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      
+      width: MediaQuery.of(context).size.width,
+      height: MediaQuery.of(context).size.height,
+      alignment: Alignment.topCenter,
+      child: Container(
+        alignment: Alignment.center,
+        margin: EdgeInsets.only(top: 80),
+        width: 600,
+        height: 100,
+        color: Colors.white,
+        child: const Text(
+          
+          'Error..',
+          textAlign: TextAlign.center,
+          style: TextStyle(fontSize: 60),
+        ),
+      ),
     );
   }
 }
