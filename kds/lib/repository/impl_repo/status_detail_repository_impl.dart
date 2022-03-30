05bac783b664ddb3f25d9292a0c3af822f0e61b1
Manuel Exposito <exposito.heman21@triana.salesianos.edu>
2022-03-30 12:36:07 +0200

    avances en estados

--- kds/lib/repository/impl_repo/status_detail_repository_impl.dart
+++ kds/lib/repository/impl_repo/status_detail_repository_impl.dart
@@ -5,26 +5,22 @@ import 'package:kds/models/status/detail_dto.dart';
 import 'package:kds/repository/repository/status_detail_repository.dart';
 import 'package:kds/utils/constants.dart';
 
-
-class StatusDetailRepositoryImpl extends StatusDetailRepository{
-
+class StatusDetailRepositoryImpl extends StatusDetailRepository {
   @override
-  Future<DetailDto> statusDetail(DetailDto detailDto) async{
+  Future<DetailDto> statusDetail(DetailDto detailDto) async {
     String url = 'http://$apiBaseUrl:$puertoKDS/modifyDetailKDS';
     Map<String, String> headers = {
-      "callback": "getLastOrders",
       "Access-Control-Allow-Origin": "*",
-      "Content-Type": "text/plain"
+      "Content-Type": "application/x-www-form-urlencoded"
     };
 
-    final response = await http.post(Uri.parse(url), headers: headers, body: jsonEncode(detailDto.toJson()));
-
-    if(response.statusCode == 200){
+    final response = await http.post(Uri.parse(url),
+        headers: headers, body: detailDto.toJson());
+    
+    if (response.statusCode == 200) {
       return detailDto;
-    }else{
+    } else {
       throw Exception('Fail to load');
     }
   }
-
-  
 }
