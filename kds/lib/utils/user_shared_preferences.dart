44f2abbdb83b52175edaf0ee2e15b9d8560b1ff5
Manuel Exposito <exposito.heman21@triana.salesianos.edu>
2022-04-28 18:05:02 +0200

    temporizador para mostrar_ultimo_tiempo terminado

--- kds/lib/utils/user_shared_preferences.dart
+++ kds/lib/utils/user_shared_preferences.dart
@@ -1,34 +1,44 @@
-
-
 import 'package:shared_preferences/shared_preferences.dart';
 
-class UserSharedPreferences{
-
+class UserSharedPreferences {
   static late SharedPreferences _prefs;
 
-
-  static Future<void> setResumeCall(String newResumeCall) async{
-
+  static Future<void> setResumeCall(String newResumeCall) async {
     _prefs = await SharedPreferences.getInstance();
 
     await _prefs.setString("resumeCall", newResumeCall);
-
   }
 
-  static Future<String> getResumeCall() async{
+  static Future<String> getResumeCall() async {
     _prefs = await SharedPreferences.getInstance();
     final resumeCall = _prefs.getString("resumeCall");
-    
-    return  resumeCall ?? '';
-  }
-  
-  static Future<void> removeResumeCall() async{
 
+    return resumeCall ?? '';
+  }
+
+  static Future<void> removeResumeCall() async {
     _prefs = await SharedPreferences.getInstance();
 
     _prefs.remove("resumeCall");
-
   }
 
+  static Future<void> setDetailTimer(String idDetail, String timer) async {
+    _prefs = await SharedPreferences.getInstance();
 
+    await _prefs.setString("timer_$idDetail", timer);
+  }
+
+  static Future<String> getDetailTimer(String idDetail) async {
+    _prefs = await SharedPreferences.getInstance();
+
+    final getTime = _prefs.getString("timer_$idDetail");
+
+    return getTime ?? '00:00:00';
+  }
+
+  static Future<void> removeDetailTimer(String idDetail) async {
+    _prefs = await SharedPreferences.getInstance();
+
+    _prefs.remove("timer_$idDetail");
+  }
 }
